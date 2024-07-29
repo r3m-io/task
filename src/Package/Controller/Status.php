4da39d9571ea3b09f836439f4318c6ac1af132c1
remco van der Velde <remco.pc@outlook.com>
2024-07-29 16:56:12 +0200

    app r3m_io/task service

--- src/Package/Controller/Status.php
+++ src/Package/Controller/Status.php
@@ -5,14 +5,37 @@ namespace Package\R3m\Io\Task\Controller;
 use R3m\Io\App;
 
 use R3m\Io\Module\Controller;
+use R3m\Io\Module\File;
+
+use Exception;
 
 class Status extends Controller {
     const DIR = __DIR__ . '/';
     const MODULE_INFO = 'Info';
 
 
+    /**
+     * @throws Exception
+     */
     public static function youtube(App $object)
     {
-        ddd($object->request());
+        $timer = 0;
+        $status_url = false;
+        while(true){
+            $timer++;
+            if(File::exist($object->request('status.url'))){
+                $status_url = $object->request('status.url');
+                break;
+            }
+            sleep(1);
+            if($timer > 10){
+                echo 'Timeout' . PHP_EOL;
+                break;
+            }
+        }
+        if($status_url){
+            $read = File::read($status_url);
+            ddd($read);
+        }
     }
 }
