e50aa3642e149b74a9464e21253f527e6d9555c4
remco van der Velde <remco.pc@outlook.com>
2024-07-30 16:02:32 +0200

    app r3m_io/task service

--- src/Package/Controller/Status.php
+++ src/Package/Controller/Status.php
@@ -36,9 +36,13 @@ class Status extends Controller {
         if($status_url){
             $read = false;
             $previous = null;
+            $release_timer = false;
             while(true){
                 $read = File::read($status_url);
-                if($previous === $read){
+                if(
+                    $previous === $read &&
+                    $release_timer === false
+                ){
                     $timer++;
                 } else {
                     $timer = 0;
@@ -46,8 +50,104 @@ class Status extends Controller {
                 if(str_contains($read, 'yt-dlp: not found')){
                     throw new Exception('Please install the yt-dlp package');
                 }
-                $content = explode(PHP_EOL, $read);
-                d($content);
+                if(str_contains($read, '[download]')){
+                    //release timer
+                    $release_timer = true;
+                }
+                if(str_contains($read, '[finished]')){
+                    //release timer
+                    break;
+                }
+//                $content = explode(PHP_EOL, $read);
+                if($release_timer){
+                    $read_line =  File::tail($status_url);
+                    //[download]   1.7% of  228.97MiB at    5.83MiB/s ETA 00:38
+                    $percentage = null;
+                    $download = null;
+                    $download_format = null;
+                    $size = null;
+                    $size_format = null;
+                    $speed = null;
+                    $speed_format = null;
+                    $eta = null;
+                    $explode = explode('[download]', $read_line, 2);
+                    if(array_key_exists(1, $explode)){
+                        $explode = explode('%', $explode[1], 2);
+                        if(array_key_exists(0, $explode)){
+                            $percentage = (float) trim($explode[0]);
+                            if(array_key_exists(1, $explode)){
+                                $explode = explode('of', $explode[1], 2);
+                                if(array_key_exists(1, $explode)){
+                                    $explode = explode('at', $explode[1], 2);
+                                    if(array_key_exists(0, $explode)){
+                                        $size = trim($explode[0]);
+                                        $size = File::size_calculation($size);
+                                        $size_format = File::size_format($size);
+                                        $download = $size * ($percentage / 100);
+                                        $download_format = File::size_format($download);
+                                    }
+                                    if(array_key_exists(1, $explode)){
+                                        $explode = explode('/s ETA', $explode[1], 2);
+                                        if(array_key_exists(0, $explode)){
+                                            $speed = trim($explode[0]);
+                                            $speed = File::size_calculation($speed);
+                                            $speed_format = File::size_format($speed) . '/s';
+                                        }
+                                        if(array_key_exists(1, $explode)) {
+                                            $eta = trim($explode[1]);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        $progress = (object) [
+                            'percentage' => $percentage,
+                            'download' => $download,
+                            'download_format' => $download_format,
+                            'size' => $size,
+                            'size_format' => $size_format,
+                            'speed' => $speed,
+                            'speed_format' => $speed_format,
+                            'eta' => $eta,
+                            'read' => $read_line
+                        ];
+                        d($progress);
+                        //progress needs to be added to the task through a patch
+                    }
+                    $explode =  explode('[ExtractAudio]', $read_line, 2);
+                    if(array_key_exists(1, $explode)) {
+                        $explode = explode('[download]', $read, 2);
+                        $basename = false;
+                        if (array_key_exists(1, $explode)) {
+                            $explode = explode(':', $explode[1], 2);
+                            if (array_key_exists(1, $explode)) {
+                                $explode = explode(PHP_EOL, $explode[1]);
+                                $basename = trim($explode[0]);
+                            }
+                        }
+                        $tmp = explode('.', $basename);
+                        $extension = array_pop($tmp);
+                        $basename = implode('.', $tmp);
+                        $target = $object->request('node.target.dir') . $basename . $object->config('extension.mp3');
+                        $size = 0;
+                        if (File::exist($target)) {
+                            $size = File::size($target);
+                            $size_format = File::size_format($size);
+                        }
+//                        $read= File::tail($status_url);
+                        $progress = (object)[
+                            'percentage' => 100,
+                            'is_converting' => true,
+                            'target' => $target,
+                            'destination_size' => $size,
+                            'destination_size_format' => $size_format,
+                            'extension' => $extension,
+                            'read' => $read_line
+                        ];
+                        d($progress);
+                        //progress needs to be patched in the task
+                    }
+                }
                 sleep(1);
                 $previous = $read;
                 if($timer > 300){
