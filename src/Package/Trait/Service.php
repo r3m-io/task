adb05354ddfae3e2d24b610e5976b8ed13a05d98
remco van der Velde <remco.pc@outlook.com>
2024-07-30 18:07:45 +0200

    app r3m_io/task service

--- src/Package/Trait/Service.php
+++ src/Package/Trait/Service.php
@@ -183,7 +183,7 @@ trait Service {
                                 $status = Core::deep_clone($object);
                                 $status->request($record->get('options.request'));
                                 $status->request('task', $task);
-                                $result = $route->controller::{$route->function}($object);
+                                $result = $route->controller::{$route->function}($status);
 
                                 d($result);
                                 exit();
